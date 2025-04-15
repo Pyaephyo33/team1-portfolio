df88ebec8a2665d18a5e520f73a0590392f0fcda
Enoch Wong <enochwong@Enochs-MacBook-Pro.local>
2025-04-15 12:07:18 +0100

    Team data, logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo_light.png";
 import { FaXmark, FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
